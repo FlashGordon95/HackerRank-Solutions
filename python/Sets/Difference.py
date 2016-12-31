4e12db2b11c19864d2ae8c204fa40a8398e9a6c6
FlashGordon95 <ryangordon210@gmail.com>
2016-12-31 23:40:02 +0000

    Difference
    Same code as intersection except swap intersection for difference

---
+++ python/Sets/Difference.py
@@ -0,0 +1,13 @@
+"""
+
+
+"""
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+s = set(map(int, input().split()))
+m = int(input())
+b = set(map(int, input().split()))
+
+print(len(s.difference(b)))
