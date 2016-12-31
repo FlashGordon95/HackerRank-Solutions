e32d3e9da18e1930f8970e20766cb46b34f0f322
FlashGordon95 <ryangordon210@gmail.com>
2016-12-31 23:39:34 +0000

    Set intersections
    Did the intersections challenge in 5 lines

---
+++ python/Sets/Intersections.py
@@ -0,0 +1,16 @@
+"""
+
+
+
+"""
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+s = set(map(int, input().split()))
+m = int(input())
+b = set(map(int, input().split()))
+
+print(len(s.intersection(b)))
+
+
